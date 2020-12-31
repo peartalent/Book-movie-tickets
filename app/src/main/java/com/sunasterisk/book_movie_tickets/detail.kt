fa336c60d79b7bb61d10cc6071e69db6f10554b2
peartalent <peartalent@gmail.com>
2020-12-31 21:19:45 +0700

    ud

---
+++ app/src/main/java/com/sunasterisk/book_movie_tickets/detail.kt
@@ -0,0 +1,12 @@
+package com.sunasterisk.book_movie_tickets
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class detail : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+        
+    }
+}
